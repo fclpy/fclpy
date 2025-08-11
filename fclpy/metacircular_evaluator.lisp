fcc94304318b5f720e5c2b55aad03f5e419a2123
Ralph Ritoch <rritoch@gmail.com>
2025-08-11 11:05:34 +0800

    Moved lisp file

---
+++ fclpy/metacircular_evaluator.lisp
@@ -0,0 +1,5 @@
+;; MIT License
+;; Copyright (c) 2019-2025 Ralph Ritoch
+;;
+;; A metacircular evaluator for a subset of Lisp in Lisp.
+;; ...existing code from previous metacircular_evaluator.lisp file...
